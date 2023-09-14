fa9cd820b9d37aa1f20bd5fbb3a7c53c5981ef61
redwan9011 <redwan390309@gmail.com>
2023-09-14 20:25:58 +0600

    i complete the blog section with using usestate and useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-   <div className='max-w-6xl mx-auto'>
+   <div className='max-w-6xl mx-auto mb-6'>
     <div className='text-center text-4xl font-bold my-8'>
     <h1>Course Registration</h1>
     </div>
--- src/Components/blogs/Blogs.jsx
+++ src/Components/blogs/Blogs.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
+import profile from '../../assets/Frame.svg'
 
 const Blogs = () => {
     // eslint-disable-next-line no-unused-vars
@@ -14,12 +14,23 @@ const Blogs = () => {
 
     return (
         <div className="w-10/12">
-            <div className="blogs grid grid-cols-3 ">
+            <div className="blogs grid grid-cols-3 gap-5 ">
                 {
                     blogs.map((blog, idx) =>(
-                      <div key={idx}>
-                        <img src={blog.cover_image} alt="" />
-                        <h1>{blog.title}</h1> 
+                      <div key={idx} className="shadow-2xl bg-white px-3 py-5 space-y-2 rounded-md">
+                        <img className="w-full" src={blog.cover_image} alt="" />
+                        <h1 className="font-bold text-lg">{blog.title}</h1> 
+                        <p className="text-gray-600"> {blog.mini_text} </p>
+                        <div className="flex justify-between text-xl text-gray-800 ">
+                            <small>$ price: {blog.price}</small>
+                           <div className="flex gap-2">
+                           <img src={profile} alt="" />
+                            <small>  Credit:  {blog.time_in_hours}hr</small>
+                           </div>
+                        </div>
+                      <div className="pt-2 text-center">
+                      <button className="w-11/12 rounded-md bg-blue-600 py-1 text-white text-normal ">Select</button>
+                      </div>
                       </div>
 
                     ))
